a9b656a0557c9e6260668b2fa017948bec57ef46
sankaillangakoon <147160063+sankaillangakoon@users.noreply.github.com>
2023-12-06 12:54:46 +0000

    Update Cloudrun.py

--- Cloudrun.py
+++ Cloudrun.py
@@ -3,7 +3,7 @@ import os
 
 def pubsub_trigger(event, context):
     """Triggered by a Pub/Sub message."""
-    cloud_run_url = os.getenv('CLOUD_RUN_URL')  # Set this as an environment variable
+    cloud_run_url = os.getenv('https://cloudrun-jbeso3reka-nw.a.run.app')  # Set this as an environment variable
 
     response = requests.post(f"{cloud_run_url}/run-model")  # Add any required payload
     return response.text
